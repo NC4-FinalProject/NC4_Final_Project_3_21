67a727168ae893803dbdc3e2edb20ea1716b241b
신지우 <yuljeonjww@gmail.com>
2024-03-26 16:02:15 +0900

    add : 주석처리

--- src/main/java/com/bit/nc4_final_project/controller/ChatController.java
+++ src/main/java/com/bit/nc4_final_project/controller/ChatController.java
@@ -1,13 +1,13 @@
-package com.bit.nc4_final_project.controller;
-
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ChatController {
-    
-    @MessageMapping("/chat")
-    public void sendMessage () {
-        
-    }
-}
+//package com.bit.nc4_final_project.controller;
+//
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ChatController {
+//
+//    @MessageMapping("/chat")
+//    public void sendMessage () {
+//
+//    }
+//}
